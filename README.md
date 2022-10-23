a6b15dff7748ed074032696d0b9cbd1e9f9322aa
Encore Shao <encore.shao@gmail.com>
2022-10-23 11:27:55 +0800

    misc - add images and update README

--- README.md
+++ README.md
@@ -4,5 +4,13 @@
 
 ## 列表显示
 
+[Poetry - Home](https://encoreshao.github.io/chinese-poetry)
+
+<img align='left' src="https://github.com/encoreshao/encoreshao/blob/main/images/home.png" width="400">
+
 ## 滑动查看
 
+<img align='left' src="https://github.com/encoreshao/encoreshao/blob/main/images/v1.png" width="400">
+
+
+<img align='left' src="https://github.com/encoreshao/encoreshao/blob/main/images/v2.png" width="400">
--- pages/about.tsx
+++ pages/about.tsx
@@ -5,77 +5,80 @@ import { List, ListItem, Typography, ListItemText } from '@mui/material';
 
 export default function About() {
   return (
-    <Container
-      sx={{ mt: 8, mb: 4 }}
-      maxWidth={ 'xl' }
-      id="about-container"
-    >
-      <Typography component="div" className="loading" />
+    <>
+      <Container id="loading-container">
+        <Typography component="div" className="loading" />
+      </Container>
 
-      <Typography
-        sx={{ pt: 8 }}
-        component="div"
-        align="left"
-        color="text.secondary"
-        paragraph
+      <Container
+        maxWidth={ 'xl' }
+        id="about-container"
       >
-        <List dense={true} disablePadding={true} sx={{ pt: 10, mt: 8 }}>
-          <ListItem>
-            <ListItemText
-              primary={
-                <Typography variant='h3' component='h3'>
-                  Encore Shao
-                </Typography>
-              }
-            />
-          </ListItem>
-          <ListItem sx={{ pb: 3 }}>
-            <ListItemText
-              primary={
-                <Typography variant='subtitle1' component='strong'>
-                  Principal Software Engineer At Ekohe
-                </Typography>
-              }
-            />
-          </ListItem>
-          <ListItem>
-            <ListItemText
-              primary={
-                <Typography variant='inherit' component='p'>
-                  🤔   Exploring new technologies and developing software solutions and quick hacks.
-                </Typography>
-              }
-            />
-          </ListItem>
-          <ListItem>
-            <ListItemText
-              primary={
-                <Typography variant='inherit' component='p'>
-                  🎓   Lead Software Engineer and Coding stuffs.
-                </Typography>
-              }
-            />
-          </ListItem>
-          <ListItem>
-            <ListItemText
-              primary={
-                <Typography variant='inherit' component='p'>
-                  🌱   Learning about Cloud Tech, Systems Design.
-                </Typography>
-              }
-            />
-          </ListItem>
-          <ListItem>
-            <ListItemText
-              primary={
-                <Typography variant='inherit' component='p'>
-                  ✍️   Pursuing Web Development and Open Source as hobbies.
-                </Typography>
-              }
-            />
-          </ListItem>
-        </List>
-      </Typography>
-    </Container>
+        <Typography
+          sx={{ }}
+          component="div"
+          align="left"
+          color="text.secondary"
+          paragraph
+        >
+          <List dense={true} disablePadding={true} sx={{ }}>
+            <ListItem>
+              <ListItemText
+                primary={
+                  <Typography variant='h3' component='h3'>
+                    Encore Shao
+                  </Typography>
+                }
+              />
+            </ListItem>
+            <ListItem sx={{ pb: 3 }}>
+              <ListItemText
+                primary={
+                  <Typography variant='subtitle1' component='i'>
+                    Principal Software Engineer At Ekohe
+                  </Typography>
+                }
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary={
+                  <Typography variant='inherit' component='p'>
+                    🤔   Exploring new technologies and developing software solutions and quick hacks.
+                  </Typography>
+                }
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary={
+                  <Typography variant='inherit' component='p'>
+                    🎓   Lead Software Engineer and Coding stuffs.
+                  </Typography>
+                }
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary={
+                  <Typography variant='inherit' component='p'>
+                    🌱   Learning about Cloud Tech, Systems Design.
+                  </Typography>
+                }
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary={
+                  <Typography variant='inherit' component='p'>
+                    ✍️   Pursuing Web Development and Open Source as hobbies.
+                  </Typography>
+                }
+              />
+            </ListItem>
+          </List>
+        </Typography>
+      </Container>
+    </>
   );
 }
